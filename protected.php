767310be0b1770cc1ffa88eb0086b18200afd3e5
shriya17 <shriya.mishra1704@gmail.com>
2015-08-12 22:18:25 +0530

    Create protected.php

---
+++ protected.php
@@ -0,0 +1,11 @@
+<?php 
+	include'core/init.php'; 
+	include'includes/overall/header.php'; ?>
+	
+		<h1>You need to login to do that !</h1>
+		<p>Please register or login to access this page.</p>	
+ <p style="color:RoyalBlue;font-size:15px;opacity:0.9;background-color:lightblue"> This site is an effort to provide the maximum information to the CETians regarding the curriculum of their college.Beside that it allows both teacher and student side involvement for the same.</p>
+   
+		
+    </div>
+		<?php include'includes/overall/footer.php'; ?>
