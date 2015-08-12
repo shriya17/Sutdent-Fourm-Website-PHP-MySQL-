be1ad2867b0bcd53a9273a7f92a6db985c2a5378
shriya17 <shriya.mishra1704@gmail.com>
2015-08-12 22:13:59 +0530

    Rename activity.php to activity1.php

---
+++ activity1.php
@@ -0,0 +1,13 @@
+<?php 
+	include'core/init.php'; 
+	protect_page();
+	admin_protect();
+	include'includes/overall/header.php'; 
+	include 'includes/activityclub.php';?>
+	
+		
+
+
+		
+    </div>
+		<?php include'includes/overall/footer.php'; ?>
