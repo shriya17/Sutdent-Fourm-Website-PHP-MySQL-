420b2b9d0be397bd7f577607777888831a966fa5
shriya17 <shriya.mishra1704@gmail.com>
2015-08-12 22:14:27 +0530

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php 
+	include'core/init.php'; 
+	protect_page();
+	admin_protect();
+	include'includes/overall/header.php'; ?>
+	
+		<h1>Admin</h1>
+		<p>Admin List</p>	
+
+
+		
+    </div>
+		<?php include'includes/overall/footer.php'; ?>
